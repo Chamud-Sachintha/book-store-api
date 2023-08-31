dd90ca528b45907c2271c836e1cb08d3dc7c311b
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-08-31 22:30:26 +0530

    jwt token implementation

---
+++ app/Http/Controllers/ClientContoller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientContoller extends Controller
+{
+    public function sayHellow() {
+        return "Hellow jax";
+    }
+}
