24a268f8aac84c5b6ddbe21749801ef2a7682c09
chamudsachintha <sheraanmario777@gmail.com>
2023-11-02 16:03:52 +0530

    update mail view

--- resources/views/emails/auth_code.blade.php
+++ resources/views/emails/auth_code.blade.php
@@ -12,7 +12,7 @@
     <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
         <div style="margin:50px auto;width:70%;padding:20px 0">
             <div style="border-bottom:1px solid #eee">
-                <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Your Brand</a>
+                <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">dpuremaths</a>
             </div>
             <p style="font-size:1.1em">Hi,</p>
             <p>We received a request to reset your dpuremaths password. Please use the One-Time Password (OTP) below to continue the process.</p>
